a46271a986a8cd794dd42d8df9c0c6c3e021306c
Jason C Miller <cyzicusgit@gmail.com>
2016-12-28 20:40:52 -0700

    completes Lab 1 - Part 6

--- src/main/java/com/cyzicus/data/repo/PlayerRepository.java
+++ src/main/java/com/cyzicus/data/repo/PlayerRepository.java
@@ -1,11 +1,11 @@
 package com.cyzicus.data.repo;
 
-import com.cyzicus.data.entity.TeamEntity;
+import com.cyzicus.data.entity.PlayerEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RestResource(path = "teams", rel = "team")
-public interface TeamRepository extends CrudRepository<TeamEntity, Long> {
+@RestResource(path = "players", rel = "player")
+public interface PlayerRepository extends CrudRepository<PlayerEntity, Long>{
 }
